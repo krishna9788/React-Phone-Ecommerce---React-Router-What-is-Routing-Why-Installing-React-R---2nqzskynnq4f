c59d4923cca64e8a2722011a06e9e07ecd2900e1
krishna9788 <krishnakushwaha978@gmail.com>
2023-03-15 23:42:14 +0530

    assign

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,12 +6,25 @@ import styled from "styled-components";
 import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
   state = {
-    products: storeProducts
+    products: storeProducts,
   };
   render() {
     return (
       <React.Fragment>
-        START HACKING
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
       </React.Fragment>
     );
   }
